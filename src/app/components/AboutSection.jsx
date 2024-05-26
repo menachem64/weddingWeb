d2110d13a3f3d5ec4319880e94412879a97f1767
menachem64 <mnsudak02@gmail.com>
2024-05-26 14:57:03 +0300

    fix mb

--- src/app/components/AboutSection.jsx
+++ src/app/components/AboutSection.jsx
@@ -48,7 +48,7 @@ const AboutSection = () => {
 
   return (
     <section className="text-white" id="about">
-      <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
+      <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16 mb-10 md:mb-0">
         {/* <Image src="/images/about-image.png" width={500} height={500} /> */}
            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3383.7900025651984!2d34.86726042607474!3d31.993707723607045!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1502caa7c87cf23d%3A0x670cb3df33138ab0!2z15DXldec150g15DXmdeo15XXoteZ150g15HXmdeqINeo15HXp9eUINeb16TXqCDXl9eR15M!5e0!3m2!1siw!2sil!4v1715621747233!5m2!1siw!2sil"
           className="hidden md:block w-[330px] h-[330px] md:w-[360] md:h-[360] xl:w-[400px] xl:h-[400px] border shadow-xl" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
@@ -62,7 +62,7 @@ const AboutSection = () => {
           </p>
           <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3383.7900025651984!2d34.86726042607474!3d31.993707723607045!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1502caa7c87cf23d%3A0x670cb3df33138ab0!2z15DXldec150g15DXmdeo15XXoteZ150g15HXmdeqINeo15HXp9eUINeb16TXqCDXl9eR15M!5e0!3m2!1siw!2sil!4v1715621747233!5m2!1siw!2sil"
             className="block md:hidden w-[240px] h-[200px] md:w-[400px] md:h-[400px]" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-          <div className="flex flex-row justify-end text-[13px] text-center md:text-xl md:text-end mt-20 mb-20 md:mb-0 w-auto">
+          <div className="flex flex-row justify-end text-[13px] text-center md:text-xl md:text-end mt-20 w-auto">
             <TabButton
               selectTab={() => handleTabChange("ShabatSevaBrachot")}
               active={tab === "ShabatSevaBrachot"}
