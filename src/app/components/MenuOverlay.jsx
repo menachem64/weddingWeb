ef530a65aa98a71d968181ac1e9d972a6003d29c
menachem64 <mnsudak02@gmail.com>
2024-06-03 17:14:54 +0300

    reverse to menuOverlay

--- src/app/components/MenuOverlay.jsx
+++ src/app/components/MenuOverlay.jsx
@@ -4,7 +4,12 @@ import NavLink from "./NavLink";
 const MenuOverlay = ({ links }) => {
   return (
     <ul className="flex flex-col py-4 items-center">
-      {links.map((link, index) => (
+      {/* {links.map((link, index) => (
+        <li key={index}>
+          <NavLink href={link.path} title={link.title} />
+        </li>
+      ))} */}
+      {links.slice().reverse().map((link, index) => (
         <li key={index}>
           <NavLink href={link.path} title={link.title} />
         </li>
