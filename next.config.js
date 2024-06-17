9dc6b37b58866cf2524c0aeedfedae770cf1c009
menachem64 <mnsudak02@gmail.com>
2024-06-16 22:16:52 -0400

    added PWA

--- next.config.js
+++ next.config.js
@@ -1,4 +1,24 @@
+// const withPWA = require('next-pwa')({
+//     dest: 'public'
+//   })
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
 
-module.exports = nextConfig
+// module.exports = withPWA({
+//     pwa: {
+//         dest: "public",
+//         register: true,
+//         skipWaiting: true,
+//     }
+// });
+
+const withPWA = require('next-pwa')({
+    dest: 'public',
+    disable: process.env.NODE_ENV === 'development'
+  });
+  
+  module.exports = withPWA({
+    reactStrictMode: true,
+    // הגדרות נוספות במידת הצורך
+  });
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,45 +1,32 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
-import { ToastProvider } from './components/toaster-provider'
-import Head from 'next/head'; 
+import './globals.css';
+import { Inter } from 'next/font/google';
+import { ToastProvider } from './components/toaster-provider';
+import Head from 'next/head';
 
-
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
   title: 'החתונה',
   description: 'החתונה של מענדי וחני',
   icons: {
     icon: '/w.ico'
-  }
+  },
+  manifest: '/manifest.json'
 }
-
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-        <Head>
+      <Head>
         <title>{metadata.title}</title>
         <meta name="description" content={metadata.description} />
         <link rel="icon" href={metadata.icons.icon} />
-
-        {/* Open Graph Meta Tags */}
-        <meta property="og:title" content={metadata.title} />
-        <meta property="og:description" content={metadata.description} />
-        <meta property="og:image" content="/weddingLogo2" /> {/* Change to the actual image path */}
-        <meta property="og:url" content="https://wedding-web-eta.vercel.app" /> {/* Change to your actual website URL */}
-        <meta property="og:type" content="website" />
-
-        {/* Twitter Card Meta Tags */}
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:title" content={metadata.title} />
-        <meta name="twitter:description" content={metadata.description} />
-        <meta name="twitter:image" content='/weddingLogo2' /> {/* Change to the actual image path */}
+        <link rel="manifest" href="/manifest.json" />
       </Head>
       <body className={inter.className}>
-        <ToastProvider/>
+        <ToastProvider />
         {children}
-        </body>
+      </body>
     </html>
-  )
+  );
 }
